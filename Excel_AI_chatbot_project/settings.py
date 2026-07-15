7fc83a20a5d66794cb689ea96376264397d17ec1
Urvesh004 <urveshmovaliya@gmail.com>
2026-07-15 09:34:29 +0530

    update database

--- Excel_AI_chatbot_project/settings.py
+++ Excel_AI_chatbot_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5k2(hjyq+y*l*281ce$(lx=dcf%@(5d!9t0jy*6ua1-kr0lg%!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "teacher-ai-chatbot-chi.vercel.app",
